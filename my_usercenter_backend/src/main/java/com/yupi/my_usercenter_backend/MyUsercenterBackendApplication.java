2d7b1cbdd9a98c1a051a5ee40450a5fe3ef8dbcf
li-changbao <2500822924@qq.com>
2024-04-07 10:20:31 +0800

    初始化后端框架

---
+++ my_usercenter_backend/src/main/java/com/yupi/my_usercenter_backend/MyUsercenterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.my_usercenter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUsercenterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUsercenterBackendApplication.class, args);
+	}
+
+}
---
+++ my_usercenter_backend/src/test/java/com/yupi/my_usercenter_backend/MyUsercenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.my_usercenter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUsercenterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
